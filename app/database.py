e07dc3a35f4020c049e9fa1db1b026d33359902a
Katebt <72352552+Katebt@users.noreply.github.com>
2025-05-29 17:19:38 +0300

    super

--- app/database.py
+++ app/database.py
@@ -6,9 +6,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 #inrender
-DATABASE_URL = "postgresql://maintenance_db_f0k5_user:PkOy1CqZlB9uKpFQigKVhIv04uzOcozr@dpg-d0s12eh5pdvs7393ervg-a/maintenance_db_f0k5"
+#DATABASE_URL = "postgresql://maintenance_db_f0k5_user:PkOy1CqZlB9uKpFQigKVhIv04uzOcozr@dpg-d0s12eh5pdvs7393ervg-a/maintenance_db_f0k5"
 #DB_PATH = os.path.abspath("./data/test.db")
-#DATABASE_URL = "postgresql://postgres:Post123@localhost:5431/katebalbalawi"
+DATABASE_URL = "postgresql://postgres:Post123@localhost:5431/katebalbalawi"
 
 # إذا أردت جعل الاتصال ديناميكي (من env):
 # DATABASE_URL = os.getenv("DATABASE_URL", f"sqlite:///{DB_PATH}")
