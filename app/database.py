59b5925b9ab1731f650b3ff65fd0c89c1e9cd41e
Katebt <72352552+Katebt@users.noreply.github.com>
2025-06-24 11:14:23 +0300

    pass, email. roles, db

--- app/database.py
+++ app/database.py
@@ -1,24 +1,22 @@
+# database.py
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
-load_dotenv()
-#inrender
-#DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:Post123@localhost:5431/katebalbalawi")
-#ONLINEHOS
-DATABASE_URL = "postgresql://maintenance_db_f0k5_user:PkOy1CqZlB9uKpFQigKVhIv04uzOcozr@dpg-d0s12eh5pdvs7393ervg-a/maintenance_db_f0k5"
-#DB_PATH = os.path.abspath("./data/test.db")
-#DATABASE_URL = "postgresql://postgres:Post123@localhost:5431/katebalbalawi"
 
-# إذا أردت جعل الاتصال ديناميكي (من env):
-# DATABASE_URL = os.getenv("DATABASE_URL", f"sqlite:///{DB_PATH}")
-# فقط أزل connect_args عند استخدام Postgres أو أي قاعدة غير SQLite
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("❌ DATABASE_URL not set in environment variables.")
+
 if DATABASE_URL.startswith("sqlite"):
     engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 else:
     engine = create_engine(DATABASE_URL)
-print(f"Database connected at: {DATABASE_URL}")
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+print(f"✅ Database connected at: {DATABASE_URL}")
